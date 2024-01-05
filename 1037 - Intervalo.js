ec003ead32b5a46f9778b0a9f0b7aa9934a68cb7
débora <deeborabarbosab@gmail.com>
2024-01-05 14:26:16 -0300

    Update 1037 - Intervalo.js

--- 1037 - Intervalo.js
+++ 1037 - Intervalo.js
@@ -1,12 +1,3 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-
-/**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
- */
-
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
  
